487522343d2c2a2c7ac341e6fd4b5b8dcec39eb0
Akashchandra62 <akashchandra8544@gmail.com>
2023-07-19 20:53:29 +0530

    home screen completed

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/screens/screens.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        useMaterial3: true,
+      ),
+      initialRoute: '/',
+      routes: {
+        HomeScreen.routeName: (context) => const HomeScreen(),
+        DiscoverScreen.routeName: (context) => const DiscoverScreen(),
+        ArticleScreen.routeName: (context) => const ArticleScreen(),
+      },
+    );
+  }
+}
---
+++ lib/screens/screens.dart
@@ -0,0 +1,3 @@
+export 'package:news_app/screens/article_screen.dart';
+export 'package:news_app/screens/discover_screen.dart';
+export 'package:news_app/screens/home_screen.dart';
